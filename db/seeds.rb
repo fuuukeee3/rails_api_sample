d32e2ef2e17cdec89b31cf51fcc480dd3ff13f12
fu <semarusika@gmail.com>
2022-07-18 07:44:06 +0900

    create user model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+(1..2).each do |i|
+  User.create!(
+    access_token: "token#{i}",
+    api_rate_limit: i * 10
+  )
+end
